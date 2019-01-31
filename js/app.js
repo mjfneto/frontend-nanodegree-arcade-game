228b0d06645024d0739666500302a5ba4ceb208e
Mario Freire <huege.design@gmail.com>
2019-01-31 10:39:16 -0200

    feat: adds player instance and positions it

--- js/app.js
+++ js/app.js
@@ -25,12 +25,29 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.x = 202;
+    this.y = 249 + 83 / 2;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy = new Enemy();
 
+var allEnemies = [];
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
